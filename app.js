ac106dd45498540ec1a1106bcea987df97390ae6
Stefan Truong <stefan.truong@gmail.com>
2021-11-06 15:59:31 +0100

    add: refactoring

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 createDiv()
 
-const squares = document.querySelectorAll('.grid div')
-const scoreDisplay = document.querySelector('span') // score will be displayed in the span container see index.html
+const squares = document.querySelectorAll('.grid div')  // divs are now handled as an array
+const scoreDisplay = document.querySelector('span')     // score will be displayed in the span container see index.html
 const startBtn = document.querySelector('.start')
 
-const width = 10 // for pressing up or down in a 10x10 grid
+const width = 10    // for pressing up or down in a 10x10 grid
 let currentIndex = 0
 let appleIndex = 0
 let currentSnake = [2,1,0] // div index 2: head, :1 body, :0 tail
-let direction = 1 // snake goes only one block
-let score = 0
-let speed = 1
+let direction = 1   // snake goes only one block
+let score = 0       
+let speed = 1       // should be constant for now
 let intervalTime = 0
 let interval = 0
 
@@ -38,11 +38,11 @@ function startGame(){
     intervalTime = 1000
     currentSnake = [2,1,0]
     currentIndex = 0
-    currentSnake.forEach(index => squares[index].classList.add('snake'))
-    interval = setInterval(moveOutcomes, intervalTime)
+    currentSnake.forEach(index => squares[index].classList.add('snake'))    // this is where the snake at start is displayed
+    interval = setInterval(moveOutcomes, intervalTime)      // moveOutcomes will be called each 1 second
 }
 
-// deals with Collisions
+// deals with Movements and evtl Collision
 function moveOutcomes(){
 
     // BorderCollision
@@ -57,23 +57,23 @@ function moveOutcomes(){
     }
 
     // Snake moves. Tail gets removed. Head goes into right direction by adding it to the left side of the array
-    const tail = currentSnake.pop()
-    squares[tail].classList.remove('snake')
-    currentSnake.unshift(currentSnake[0] + direction)
+    const tail = currentSnake.pop()                     // pop most right element of the array away    
+    squares[tail].classList.remove('snake')             // div representing tail should be normal again
+    currentSnake.unshift(currentSnake[0] + direction)   // unshifts new element on the left
 
     // Snake eating apple
     if(squares[currentSnake[0]].classList.contains('apple')){
         squares[currentSnake[0]].classList.remove('apple')
-        squares[tail].classList.add('snake')
-        currentSnake.push(tail)
+        squares[tail].classList.add('snake')            
+        currentSnake.push(tail)                         // snake gets bigger
         randomApple()
         score++
         scoreDisplay.innerHTML = score
         clearInterval(interval)
-        intervalTime = intervalTime * speed
+        intervalTime = intervalTime * speed             // if snake should move faster change speed here
         interval = setInterval(moveOutcomes,intervalTime)
     }
-    squares[currentSnake[0]].classList.add('snake')
+    squares[currentSnake[0]].classList.add('snake')     // if all is OK new head will be displayed in the right direction via div
 }
 
 function randomApple(){
